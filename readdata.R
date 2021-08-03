83e82e44bee80ae400e9ec5b7b87260464a3a51e
javierf2 <63899353+javierf2@users.noreply.github.com>
2021-08-02 20:15:27 -0700

    Add files via upload

--- readdata.R
+++ readdata.R
@@ -5,5 +5,4 @@ library(stringr)
 library(tidyverse)
 library(plyr)
 
-
-varsdata <- read.csv(file = "https://raw.githubusercontent.com/javierf2/COVID/c93cfaca70d078691a6c7e6fba5b3a7998376140/NonDomesticVAERSVAX.csv", head = TRUE, sep=",")
+varsdata <- read.csv(file = "https://shiny11964.s3.us-west-1.amazonaws.com/2021VAERSDATA.csv")
